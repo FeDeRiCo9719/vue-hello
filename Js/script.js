f9beb8e24e6d0897be15d328152e56ce4dc4dd95
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-09-16 16:57:22 +0200

    title message

--- Js/script.js
+++ Js/script.js
@@ -5,3 +5,13 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */
+
+// 1. messaggio all’interno di un h1
+var app = new Vue({
+    el: '#Sms',
+    data: {
+        message: 'Hello world!',
+    }
+});
+
+// 2. Aggiungere alla pagina un’immagine
